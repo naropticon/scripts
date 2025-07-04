0e2fec3d26181be3b648a6177b156d65d4ad74cb
NaRa <naropticon@outlook.com>
2025-07-04 10:25:55 +0200

    Init search-AD-group.ps1
    PowerShell script AD csoportnév eleji keresésre (modul nélkül)

---
+++ search-AD-group.ps1
@@ -0,0 +1,20 @@
+# Módosítsd ezt a változót arra, amivel kezdődik a keresett csoportnév
+$keresettEleje = "dev"
+
+# AD keresés inicializálása
+$kereso = New-Object DirectoryServices.DirectorySearcher
+$kereso.Filter = "(&(objectCategory=group)(name=$keresettEleje*))"
+$kereso.PageSize = 1000  # biztosítja, hogy ne korlátozza 100 találatra
+$kereso.PropertiesToLoad.Add("name") | Out-Null
+
+# Találatok kiírása
+$talalatok = $kereso.FindAll()
+
+if ($talalatok.Count -eq 0) {
+    Write-Host "Nincs találat a(z) '$keresettEleje*' csoportnévre."
+} else {
+    Write-Host "Talált csoportok:"
+    foreach ($talalat in $talalatok) {
+        $talalat.Properties["name"]
+    }
+}
